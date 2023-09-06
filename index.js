974c2fa7c517682c4f7899972d599bef74500015
Wisnu Andika <wisnuandika104@gmail.com>
2023-09-06 12:36:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// getting random int between 1-50
+
+// generate array with default parameter 100
+
+// check and return the minimum number in each array
+
+// check and return the maximum number in each array
+
+// sum and return total value in each array
+
+// count and return average value in each array
+
+// init function
